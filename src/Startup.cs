3376a067843292e50d6fe527283a95b265bc5190
atillatan <atillatan@gmail.com>
2018-08-08 10:14:44 +0300

    Added  detailed debug log in development mode

--- src/Startup.cs
+++ src/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace SSO.Web
 {
@@ -42,10 +43,11 @@ namespace SSO.Web
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
+                loggerFactory.AddConsole(LogLevel.Trace);
                 app.UseDeveloperExceptionPage();
             }
             else
